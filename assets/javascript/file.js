d8794fd099d0b7f22b0d2d029bd49d936df108a5
Marcus Waters <marcus.waters@gmail.com>
2018-05-07 06:58:54 -0500

    commits

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -7,20 +7,15 @@ var config = {
     messagingSenderId: "357146889632"
 };
 
-
+//firebase, api, etc
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 
-
-
-
-
-$("#newButton").click(function () {
-
+$("#newButton").on("click", function(event) {
     event.preventDefault();
-
+  
     var trainName = $("#trainName").val().trim();
     console.log("trainName", trainName);
     var destination = $("#destination").val().trim();
@@ -36,51 +31,66 @@ $("#newButton").click(function () {
         time: firstTrainTime,
         frequency: frequency
     };
-
+  
+    // Uploads employee data to the database
     database.ref().push(newTrain);
-
+  
+    // Logs everything to console
     console.log(newTrain.name);
     console.log(newTrain.destination);
     console.log(newTrain.time);
     console.log(newTrain.frequency);
-
+    // Alert
     alert("New Train Added");
-
+  
+    // Clears all of the text-boxes
     $("#trainName").val("");
     $("#destination").val("");
     $("#firstTrainTime").val("");
     $("#frequency").val("");
-
-});
-
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
+  });
+  
+  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  
     console.log(childSnapshot.val());
   
-  
+    // Store everything into a variable; snapshot
     var trainName = childSnapshot.val().name;
     var destination = childSnapshot.val().destination;
     var firstTrainTime = childSnapshot.val().time;
     var frequency = childSnapshot.val().frequency;
     var firstTrainArr = firstTrainTime.split(":");
-    console.log("this is", firstTrainArr);
+    console.log("firstTrainArr", firstTrainArr);
+
+    
+    if (firstTrainTime) {
+        var nextArrival = firstTrainTime.format("hh:mm A");
+        var minutesAway = firstTrainTime.diff(moment(), "minutes");
+      } else {
+
+        
+        var differenceTimes = moment().diff(firstTrainTime, "minutes");
+        var tRemainder = differenceTimes % frequency;
+        minutesAway = frequency - tRemainder;
+        // nextArrival
+        nextArrival = moment().add(minutesAway, "m").format("hh:mm A");
+      }
+
+
 
   
-   
-    console.log(trainName);
-    console.log(destination);
-    console.log("Hello its", firstTrainTime);
-    console.log(frequency);
-
-
-  var nextArrival = 
-  console.log(nextArrival);
+    
+    var nextArrival = 
+    console.log(nextArrival);
   
-  var minutesAway = 
-  console.log(minutesAway);
-
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
-  frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td><td>");
-});
-
-
+  
+    var minutesAway = 
+    console.log(minutesAway);
+  
+    // Add each train's data into the table
+    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+    frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td><td>");
+  });
+  
+  
